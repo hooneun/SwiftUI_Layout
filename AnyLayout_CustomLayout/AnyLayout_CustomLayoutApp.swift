afa54d2fea2d5d83f9f9f6cfe12565797a9d47d3
hooneun <kimhoon0316@gmail.com>
2024-06-20 20:12:08 +0900

    init
    HStack, ZStack

---
+++ AnyLayout_CustomLayout/AnyLayout_CustomLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyLayout_CustomLayoutApp.swift
+//  AnyLayout_CustomLayout
+//
+//  Created by Hoon on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AnyLayout_CustomLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnyLayout_CustomLayout/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  AnyLayout_CustomLayout
+//
+//  Created by Hoon on 6/20/24.
+//
+
+import SwiftUI
+
+struct MyColors {
+    var color: Color
+    var width: CGFloat
+    var height: CGFloat
+
+    static var allColors: [MyColors] {
+        [
+            .init(color: .red, width: 60, height: 75),
+            .init(color: .teal, width: 100, height: 100),
+            .init(color: .purple, width: 40, height: 80),
+            .init(color: .indigo, width: 120, height: 100)
+        ]
+    }
+}
+
+struct ContentView: View {
+    @State private var changeLayout = false
+
+    var body: some View {
+        NavigationStack {
+            Group {
+                if changeLayout {
+                    HStack {
+                        ForEach(MyColors.allColors, id: \.color) { myColor in
+                            myColor.color
+                                .frame(width: myColor.width, height: myColor.height)
+                        }
+                    }
+                } else {
+                    ZStack {
+                        ForEach(MyColors.allColors, id: \.color) { myColor in
+                            myColor.color
+                                .frame(width: myColor.width, height: myColor.height)
+                        }
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("AnyLayout")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        withAnimation {
+                            changeLayout.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "circle.grid.3x3.fill")
+                            .imageScale(.large)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
